3a8e59d9b78ddfd55a52a3382c2b8b633972f910
Garry Wright <garry.wright@me.com>
2016-02-26 15:35:59 -0500

    logger

--- fileDelivery/database.go
+++ fileDelivery/database.go
@@ -4,7 +4,7 @@ import (
 	"github.com/go-martini/martini"
 	"gopkg.in/mgo.v2"
 	"os"
-	"fmt"
+	"log"
 )
 
 /*
@@ -14,29 +14,31 @@ so I'll embed *mgo.Session and store the database name.
 type DatabaseSession struct {
 	*mgo.Session
 	databaseName string
+	logger   *log.Logger
 }
 
 /*
 Connect to the local MongoDB and set up the database.
 */
 func NewSession(name string) *DatabaseSession {
+	logger := log.New(os.Stdout, "[restfiledelivery] ", 0)
 	db_server_seed := os.Getenv("DATABASE_PORT_27017_TCP_ADDR")
 
 	if db_server_seed == "" {
 		db_server_seed = "mongodb://127.0.0.1:27017"
 	}
-	
-	fmt.Printf("Dialing Mongo session at %s \n",db_server_seed)
+
+	logger.Printf("Dialing Mongo session at %s \n",db_server_seed)
 
 	// session, err := mgo.Dial("mongodb://root:root@ds057224.mongolab.com:57224/requestfiles")
 	session, err := mgo.Dial(db_server_seed)
 	if err != nil {
 		panic(err)
 	} else {
-		fmt.Printf("Mongo session established with %s %s \n",db_server_seed,name)
+		logger.Printf("Mongo session established with %s %s \n",db_server_seed,name)
 	}
 	addIndexToRequestedFiles(session.DB(name))
-	return &DatabaseSession{session, name}
+	return &DatabaseSession{session, name, logger}
 }
 
 func addIndexToRequestedFiles(db *mgo.Database) {
